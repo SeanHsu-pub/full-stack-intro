1f9739d61d17c8a27176f0b0c621aeac6c7361a4
seanhsu <shobu.pub@gmail.com>
2025-11-05 21:18:52 +0800

    add week 5

---
+++ w5/script.js
@@ -0,0 +1,11 @@
+const projectItems = document.querySelectorAll(".project-item"); // 選取所有 project 卡片
+
+projectItems.forEach(item => {
+    console.log("I'm clicked.");
+    item.addEventListener("click", function() {
+        const url = this.getAttribute("data-url"); // 在 HTML 加入的連結
+        if (url) {
+            window.open(url,"_blank"); // 在新分頁開啟
+        }
+    });
+});
